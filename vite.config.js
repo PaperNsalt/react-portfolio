ef794204f0a399520debb580d84b5e29c978875c
Jeremy O. Rellama <113158566+PaperNsalt@users.noreply.github.com>
2026-08-26 19:58:53 +0800

    vercel fix

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
   assetsInclude: ["**/*.glb"],
   plugins: [react(), tailwindcss()],
-  base: "/react-portfolio/",
+  base: "/",
 
   resolve: {
     alias: {
